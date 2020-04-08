2bfa4137e11e1d85907f109692ee4441a6c4e020
Demilade <demiladeoladugba@gmail.com>
2020-04-08 07:57:10 +0100

    Added tests and functionality for network info

---
+++ test/core/network/network_info_test.dart
@@ -0,0 +1,32 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/network/network_info.dart';
+
+///Created by Demilade Oladugba on 4/7/2020
+
+class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+
+void main() {
+  NetworkInfoImpl networkInfoImpl;
+  MockDataConnectionChecker mockDataConnectionChecker;
+
+  setUp(() {
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfoImpl = NetworkInfoImpl(mockDataConnectionChecker);
+  });
+
+  group('isConnected', () {
+    test('should forward the call to DataCOnnectionChecker.hasConnection', () async {
+      //arrange
+      final tHasConnectionFuture = Future.value(true);
+      when(mockDataConnectionChecker.hasConnection)
+          .thenAnswer((_) => tHasConnectionFuture);
+      //act
+      final result = networkInfoImpl.isConnected;
+      //asset
+      verify(mockDataConnectionChecker.hasConnection);
+      expect(result, tHasConnectionFuture);
+    });
+  });
+}
