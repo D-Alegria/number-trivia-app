6cf332591d11370788f737ce423792df2f31a13e
Demilade <demiladeoladugba@gmail.com>
2020-04-09 00:31:52 +0100

    fixed bug

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:number_trivia/features/number_trivia/presentation/pages/number_t
 import 'injection_container.dart' as di;
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await di.init();
   runApp(MyApp());
 }
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Number Trivia App',
       theme: ThemeData(
         primaryColor: Colors.green.shade800,
